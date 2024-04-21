7eb9658d07777263ff54f667f4561983a75e2e15
danimisme <msubhanr53@gmail.com>
2024-04-21 22:58:20 +0700

    modif style of card promo

--- src/component/Fragments/CardPromo/CardPromo.jsx
+++ src/component/Fragments/CardPromo/CardPromo.jsx
@@ -3,21 +3,25 @@ import { Link } from "react-router-dom";
 export default function CardPromo({ promo }) {
   return (
     <div className="col-md-3 col-sm-6 p-3">
-      <div className={`card_promo card`}>
-        <img
-          src={promo.imageUrl}
-          className={`card-img-top`}
-          style={{ width: "100%", aspectRatio: "5/3", objectFit: "cover" }}
-          alt="..."
-        />
-        <div className="card-body">
-          <h5 className="card-title">{promo.title}</h5>
-          <p className="card-text">{promo.description}</p>
-          <Link href={`/promo/${promo.id}`}>
-            <button className="btn btn-outline-success">See Details</button>
-          </Link>
+      <Link to={`/promo/${promo.id}`} className="text-decoration-none">
+        <div className={`card_promo card d-flex flex-column h-100`}>
+          <img
+            src={promo.imageUrl}
+            className={`card-img-top`}
+            style={{ width: "100%", aspectRatio: "5/3", objectFit: "cover" }}
+            alt="..."
+          />
+          <div className="card-body " style={{ backgroundColor: "#FFBB70" }}>
+            <h6 className="card-title">{promo.title}</h6>
+            <p className="card-text text-decoration-line-through">
+              {promo?.promo_discount_price?.toLocaleString("id-ID", {
+                style: "currency",
+                currency: "IDR",
+              })}
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
