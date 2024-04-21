cdd3586fbb0be94e73405de054b851fc0d60564e
danimisme <msubhanr53@gmail.com>
2024-04-22 01:09:33 +0700

    change styling for card user

--- src/component/Fragments/DashboardCardUser/CardUser.jsx
+++ src/component/Fragments/DashboardCardUser/CardUser.jsx
@@ -1,6 +1,10 @@
-export default function CardUser({ user }) {
+import "./CardUser.css";
+export default function CardUser({ user, index }) {
+  const bg = index % 3 === 0 ? "bg1" : index % 3 === 1 ? "bg2" : "bg3";
+  const bgBody =
+    index % 3 === 0 ? "bg-body1" : index % 3 === 1 ? "bg-body2" : "bg-body3";
   return (
-    <div className="card my-3">
+    <div className={`card my-3 ${bg}`}>
       <div className="w-100 d-flex justify-content-center py-3">
         <img
           src={user.profilePictureUrl}
@@ -9,19 +13,22 @@ export default function CardUser({ user }) {
           alt="profile picture"
         />
       </div>
-      <div className="card-body r d-flex flex-column gap-2">
+      <div
+        className={`card-body r d-flex flex-column gap-2 ${bgBody}`}
+        style={{ borderRadius: "10px 10px 0 0" }}
+      >
         <p className="card-text fw-semibold fs-4 border-2 border-bottom m-0 ">
           {user.name}
         </p>
-        <p className="card-text m-0">
+        <p className="card-text m-0 p-0 small">
           <i className="bi bi-envelope-fill me-1"></i>
           {user.email}
         </p>
-        <p className="card-text m-0">
+        <p className="card-text m-0 p-0 small">
           <i className="bi bi-telephone-fill me-1"></i>
           {user.phoneNumber}
         </p>
-        <p>
+        <p className="card-text m-0 p-0 small">
           <i className="bi bi-gear-fill me-1"></i>
           {user.role}
         </p>
