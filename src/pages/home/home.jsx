d175431db4b815f9b7a16b938f5552c623529cc5
danimisme <msubhanr53@gmail.com>
2024-04-21 14:13:45 +0700

    create home.css for styling

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import "./home.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="jumbotron d-flex align-items-center justify-content-center text-white ">
+        <h1>
+          Adventure to Explore <br /> Through the Beautiful World
+        </h1>
+      </div>
+    </main>
+  );
+}
