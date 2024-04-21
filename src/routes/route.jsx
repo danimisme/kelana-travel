996062c748f0a8468ee6b19f6a4b5fd3550ac2fb
danimisme <msubhanr53@gmail.com>
2024-04-21 18:21:38 +0700

    change js to jsx

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,8 +1,13 @@
 import Home from "../pages/home/home";
+import LoginPage from "../pages/login/loginPage";
 
 export const routeList = [
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   },
 ];
