aa4cf0170e69758082dd3836b15ab0e6d96d3c35
danimisme <msubhanr53@gmail.com>
2024-04-21 18:17:38 +0700

    create LoginPage

---
+++ src/pages/login/loginPage.jsx
@@ -0,0 +1,11 @@
+import LoginForm from "../../component/Fragments/LoginForm/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="row d-flex vh-100 align-items-center justify-content-center">
+      <div className="col-lg-3 col-md-6 col-10 my-5">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
