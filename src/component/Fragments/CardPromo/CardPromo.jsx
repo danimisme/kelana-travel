b7e587d1ed87ba724dd4bf0d2c07f37379c1e6a1
danimisme <msubhanr53@gmail.com>
2024-04-21 22:38:00 +0700

    create cardpromo element

---
+++ src/component/Fragments/CardPromo/CardPromo.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+export default function CardPromo({ promo }) {
+  return (
+    <div className="col-md-3 col-sm-6 p-3">
+      <div className={`card_promo card`}>
+        <img
+          src={promo.imageUrl}
+          className={`card_image card-img-top`}
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{promo.title}</h5>
+          <p className="card-text">{promo.description}</p>
+          <Link href={`/promo/${promo.id}`}>
+            <button className="btn btn-outline-success">See Details</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
