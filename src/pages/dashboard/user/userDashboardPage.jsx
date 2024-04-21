eb27cf15ead9908d2d04c283255026b39b1dd466
danimisme <msubhanr53@gmail.com>
2024-04-22 00:29:44 +0700

    add card user in user page

--- src/pages/dashboard/user/userDashboardPage.jsx
+++ src/pages/dashboard/user/userDashboardPage.jsx
@@ -1,7 +1,29 @@
-export default function UserDashboard() {
+import Layout from "../../../layouts/Layout";
+import useAuth from "../../../hooks/useAuth";
+import { useEffect, useState } from "react";
+import CardUser from "../../../component/Fragments/DashboardCardUser/CardUser";
+
+export default function UserDashboardPage() {
+  const { userLog } = useAuth();
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    userLog("all-user", setUsers);
+  }, []);
+  console.log(users);
   return (
-    <div>
-      <h1>User Dashboard</h1>
-    </div>
+    <Layout>
+      <div className="mt-5 container-lg">
+        <div className="py-5">
+          <h1>User Dashboard</h1>
+          <div className="row">
+            {users.map((user) => (
+              <div className="col-10 col-md-4 col-lg-3" key={user.id}>
+                <CardUser user={user} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 }
