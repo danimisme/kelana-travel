623c5ba2dc40e3509f7c38abf5879d2d3200cfe9
danimisme <msubhanr53@gmail.com>
2024-04-21 18:02:10 +0700

    add layout to homepage

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,20 +2,23 @@ import ActivityCarousel from "../../component/Fragments/ActivityCarousel/Activit
 import BannerCarousel from "../../component/Fragments/BannerCarousel/BannerCarousel";
 import CategoryCarousel from "../../component/Fragments/CategoryCarousel/CategortCarousel";
 import PromoCarousel from "../../component/Fragments/PromoCarousel/PromoCarousel";
+import Layout from "../../layouts/Layout";
 import "./home.css";
 
 export default function Home() {
   return (
-    <main>
-      <div className="jumbotron d-flex align-items-center justify-content-center text-white ">
-        <h1>
-          Adventure to Explore <br /> Through the Beautiful World
-        </h1>
-      </div>
-      <BannerCarousel />
-      <PromoCarousel />
-      <CategoryCarousel />
-      <ActivityCarousel />
-    </main>
+    <Layout>
+      <main>
+        <div className="jumbotron d-flex align-items-center justify-content-center text-white ">
+          <h1>
+            Adventure to Explore <br /> Through the Beautiful World
+          </h1>
+        </div>
+        <BannerCarousel />
+        <PromoCarousel />
+        <CategoryCarousel />
+        <ActivityCarousel />
+      </main>
+    </Layout>
   );
 }
