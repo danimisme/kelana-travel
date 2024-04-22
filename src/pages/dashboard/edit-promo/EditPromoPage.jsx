2d81edfa19bffa5399b812baa4364409a44c42aa
danimisme <msubhanr53@gmail.com>
2024-04-22 23:08:22 +0700

    add Form Promo to edit promo page

---
+++ src/pages/dashboard/edit-promo/EditPromoPage.jsx
@@ -0,0 +1,34 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useGetData from "../../../hooks/useGetData";
+import useUpdate from "../../../hooks/useUpdate";
+import Layout from "../../../layouts/Layout";
+import FormPromo from "../../../component/Fragments/FormPromo/FormPromo";
+
+export default function EditPromoPage() {
+  const { getData } = useGetData();
+  const [promo, setPromo] = useState({});
+
+  const { update } = useUpdate();
+  const params = useParams();
+  useEffect(() => {
+    getData(`promo/${params.id}`).then((res) => setPromo(res?.data?.data));
+  }, []);
+
+  const handleUpdatePromo = async (data) => {
+    try {
+      const res = await update(`update-promo/${params.id}`, data);
+      if (res.status === 200) {
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Layout>
+      <FormPromo promo={promo} onSubmit={handleUpdatePromo} />
+    </Layout>
+  );
+}
