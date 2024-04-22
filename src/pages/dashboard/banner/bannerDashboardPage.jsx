98641ab6ef44e682d06d55e63dcc121755deaa4e
danimisme <msubhanr53@gmail.com>
2024-04-22 12:27:48 +0700

    create banner dashboard page

---
+++ src/pages/dashboard/banner/bannerDashboardPage.jsx
@@ -0,0 +1,9 @@
+export default function BannerDashboardPage() {
+  return (
+    <div className="mt-5 container-lg">
+      <div className="py-5">
+        <h1>Banner Dashboard Page</h1>
+      </div>
+    </div>
+  );
+}
