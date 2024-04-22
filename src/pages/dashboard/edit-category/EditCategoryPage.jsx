c4f7585ccefc30ed72268371f4c8980dc2db1801
danimisme <msubhanr53@gmail.com>
2024-04-23 00:45:46 +0700

    add FormCategory to edit category page

--- src/pages/dashboard/edit-category/EditCategoryPage.jsx
+++ src/pages/dashboard/edit-category/EditCategoryPage.jsx
@@ -1,13 +1,23 @@
-import { useParams } from "next/navigation";
+import { useParams } from "react-router-dom";
 import Layout from "../../../layouts/Layout";
+import useGetData from "../../../hooks/useGetData";
+import { useEffect, useState } from "react";
+import FormCategory from "../../../component/Fragments/FormCategory/FormCategory";
 
 export default function EditCategoryPage() {
   const params = useParams();
+  const { getData } = useGetData();
+  const [categories, setCategory] = useState([]);
+  useEffect(() => {
+    getData(`category/${params.id}`).then((res) =>
+      setCategory(res?.data?.data)
+    );
+  }, []);
   return (
     <Layout>
       <div className="mt-5 container-lg">
         <div className="row py-5 align-items-center">
-          <h1>Edit Category : {params.id}</h1>
+          <FormCategory category={categories} />
         </div>
       </div>
     </Layout>
