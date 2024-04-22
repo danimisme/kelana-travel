4a3419e6da7ec2faa69ecf56bfac502cdfabce67
danimisme <msubhanr53@gmail.com>
2024-04-22 12:39:47 +0700

    useGetData to get data banners

--- src/pages/dashboard/banner/bannerDashboardPage.jsx
+++ src/pages/dashboard/banner/bannerDashboardPage.jsx
@@ -1,9 +1,20 @@
+import Layout from "../../../layouts/Layout";
+import useGetData from "../../../hooks/useGetData";
+import { useEffect, useState } from "react";
 export default function BannerDashboardPage() {
+  const { getData } = useGetData();
+  const [banners, setBanners] = useState([]);
+  useEffect(() => {
+    getData("banners").then((res) => setBanners(res.data.data));
+  }, []);
+  console.log(banners);
   return (
-    <div className="mt-5 container-lg">
-      <div className="py-5">
-        <h1>Banner Dashboard Page</h1>
+    <Layout>
+      <div className="mt-5 container-lg">
+        <div className="py-5">
+          <h1 className="text-center text-orange">Banner Data</h1>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
