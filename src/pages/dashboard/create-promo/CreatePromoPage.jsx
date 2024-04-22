8ef67339707ca5618c39ad8a64cb794a14ae2a37
danimisme <msubhanr53@gmail.com>
2024-04-22 23:51:43 +0700

    create CreatePromo Page

---
+++ src/pages/dashboard/create-promo/CreatePromoPage.jsx
@@ -0,0 +1,10 @@
+import FormPromo from "../../../component/Fragments/FormPromo/FormPromo";
+import Layout from "../../../layouts/Layout";
+
+export default function CreatePromoPage() {
+  return (
+    <Layout>
+      <FormPromo />
+    </Layout>
+  );
+}
