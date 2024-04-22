34f60d40b574d09d7621d63bf7b17a2ffca2ac47
danimisme <msubhanr53@gmail.com>
2024-04-23 00:59:42 +0700

    create handleCreate funtion for create category

--- src/pages/dashboard/create-category/CreateCategoryPage.jsx
+++ src/pages/dashboard/create-category/CreateCategoryPage.jsx
@@ -1,12 +1,24 @@
 import Layout from "../../../layouts/Layout";
 import FormCategory from "../../../component/Fragments/FormCategory/FormCategory";
+import useCreate from "../../../hooks/useCreate";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateCategoryPage() {
+  const { create } = useCreate();
+  const navigate = useNavigate();
+  const handleCreateCategory = async (data) => {
+    try {
+      await create("create-category", data);
+      navigate("/dashboard/category");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Layout>
       <div className="mt-5 container-lg">
         <div className="row py-5 align-items-center">
-          <FormCategory />
+          <FormCategory onSubmit={handleCreateCategory} />
         </div>
       </div>
     </Layout>
