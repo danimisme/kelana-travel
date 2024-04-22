0b9c84a0c6ae60554ad1d350c932a1aa1c2f1193
danimisme <msubhanr53@gmail.com>
2024-04-22 21:18:53 +0700

    add Layout in create-banner

--- src/pages/dashboard/create-banner/CreateBannerPage.jsx
+++ src/pages/dashboard/create-banner/CreateBannerPage.jsx
@@ -1,9 +1,13 @@
+import Layout from "../../../layouts/Layout";
+
 export default function CreateBannerPage() {
   return (
-    <div className="mt-5 container-lg">
-      <div className="row py-5 align-items-center">
-        <h1>Create Banner Page</h1>
+    <Layout>
+      <div className="mt-5 container-lg">
+        <div className="row py-5 align-items-center">
+          <h1>Create Banner Page</h1>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
