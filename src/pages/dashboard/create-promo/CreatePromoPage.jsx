42b9f87ef9d3503fffd0608bb05cd0ee4bc39338
danimisme <msubhanr53@gmail.com>
2024-04-23 00:03:31 +0700

    create handleCreatePromo funtion

--- src/pages/dashboard/create-promo/CreatePromoPage.jsx
+++ src/pages/dashboard/create-promo/CreatePromoPage.jsx
@@ -1,10 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import FormPromo from "../../../component/Fragments/FormPromo/FormPromo";
 import Layout from "../../../layouts/Layout";
+import useCreate from "../../../hooks/useCreate";
 
 export default function CreatePromoPage() {
+  const navigate = useNavigate();
+  const { create } = useCreate();
+  const handleCreatePromo = async (data) => {
+    try {
+      await create("create-promo", data);
+      navigate("/dashboard/promo");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Layout>
-      <FormPromo />
+      <FormPromo onSubmit={handleCreatePromo} />
     </Layout>
   );
 }
