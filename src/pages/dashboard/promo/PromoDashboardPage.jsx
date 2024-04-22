7ad466057d1fd1c2089f028b21b75272ef59d6bd
danimisme <msubhanr53@gmail.com>
2024-04-22 21:51:53 +0700

    create Promo Page

---
+++ src/pages/dashboard/promo/PromoDashboardPage.jsx
@@ -0,0 +1,13 @@
+import Layout from "../../../layouts/Layout";
+
+export default function PromoDashboardPage() {
+  return (
+    <Layout>
+      <div className="mt-5 container-lg">
+        <div className="row py-5">
+          <h1>Promo Data</h1>
+        </div>
+      </div>
+    </Layout>
+  );
+}
