91af94e194c67ae87fa5804f1c524c3ade0e1070
danimisme <msubhanr53@gmail.com>
2024-04-22 21:38:07 +0700

    create handleCreate and add form to createBannerPage

--- src/pages/dashboard/create-banner/CreateBannerPage.jsx
+++ src/pages/dashboard/create-banner/CreateBannerPage.jsx
@@ -1,11 +1,25 @@
+import FormBanner from "../../../component/Fragments/FormBanner/FormBanner";
 import Layout from "../../../layouts/Layout";
+import useCreate from "../../../hooks/useCreate";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateBannerPage() {
+  const { create } = useCreate();
+  const navigate = useNavigate();
+  const handleCreateBanner = async (data) => {
+    try {
+      await create("create-banner", data);
+      navigate("/dashboard/banner");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Layout>
       <div className="mt-5 container-lg">
         <div className="row py-5 align-items-center">
-          <h1>Create Banner Page</h1>
+          <FormBanner onSubmit={handleCreateBanner} />
         </div>
       </div>
     </Layout>
