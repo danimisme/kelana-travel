74f14463b075d379c2999156529c43c5f402d50b
danimisme <msubhanr53@gmail.com>
2024-04-22 21:11:58 +0700

    create create banner Page

---
+++ src/pages/dashboard/create-banner/CreateBannerPage.jsx
@@ -0,0 +1,9 @@
+export default function CreateBannerPage() {
+  return (
+    <div className="mt-5 container-lg">
+      <div className="row py-5 align-items-center">
+        <h1>Create Banner Page</h1>
+      </div>
+    </div>
+  );
+}
