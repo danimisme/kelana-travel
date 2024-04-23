ab03091b214b0f2abe030020a2675a3fd6a70439
danimisme <msubhanr53@gmail.com>
2024-04-24 00:40:43 +0700

    add filter activity

--- src/pages/dashboard/activity/ActivityDashboardPage.jsx
+++ src/pages/dashboard/activity/ActivityDashboardPage.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import CardActivity from "../../../component/Fragments/DashboardCardActivity/CardActivity";
 import useDelete from "../../../hooks/useDelete";
 import Pagination from "../../../component/Elements/Pagination/Pagination";
+import SelectOption from "../../../component/Elements/SelectOption/SelectOption";
 
 export default function ActivityDashboardPage() {
   const { getData } = useGetData();
@@ -15,9 +16,11 @@ export default function ActivityDashboardPage() {
   const startIndex = (page - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const totalPages = Math.ceil(activities.length / itemsPerPage);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     getData("activities").then((res) => setActivities(res.data.data));
+    getData("categories").then((res) => setCategories(res.data.data));
   }, []);
 
   const handleDelete = async (id) => {
@@ -29,6 +32,24 @@ export default function ActivityDashboardPage() {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const handleReset = () => {
+    const select = document.getElementById("select_categories");
+    select.value = "Select";
+    getData("activities").then((res) => setActivities(res.data.data));
+  };
+
+  const handleFilter = async () => {
+    const select = document.getElementById("select_categories");
+    const value = select.value;
+    if (value === "Select") {
+      handleReset();
+      return;
+    }
+    getData(`activities-by-category/${value}`).then((res) =>
+      setActivities(res.data.data)
+    );
   };
 
   return (
@@ -52,6 +73,33 @@ export default function ActivityDashboardPage() {
               </div>
             </Link>
           </div>
+
+          <div className={`d-flex justify-content-center container-lg mt-2`}>
+            <div className="d-flex align-items-center justify-content-center">
+              <div className=" d-flex align-items-center ">
+                <p className="m-0">Filter By Category</p>
+              </div>
+              <div className=" mx-3">
+                <SelectOption selectItems={categories} id="select_categories" />
+              </div>
+            </div>
+            <div className="d-flex gap-3">
+              <button
+                className="default-button btn-orange"
+                onClick={handleFilter}
+              >
+                Filter
+              </button>
+              <button
+                className="default-button "
+                style={{ backgroundColor: "gray", color: "white" }}
+                onClick={handleReset}
+              >
+                Reset
+              </button>
+            </div>
+          </div>
+
           <div className="row justify-content-center">
             {activities.slice(startIndex, endIndex).map((activity) => (
               <div className="col-lg-4 col-md-6 col-10 mt-3" key={activity.id}>
