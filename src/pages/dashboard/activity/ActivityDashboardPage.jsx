467b117b129d37242b91be3a522e824fd29f6356
danimisme <msubhanr53@gmail.com>
2024-04-23 20:28:51 +0700

    create activity dashboard page

---
+++ src/pages/dashboard/activity/ActivityDashboardPage.jsx
@@ -0,0 +1,13 @@
+import Layout from "../../../layouts/Layout";
+
+export default function ActivityDashboardPage() {
+  return (
+    <Layout>
+      <div className="container-lg mt-5">
+        <div className="py-5">
+          <h2>Activity Data</h2>
+        </div>
+      </div>
+    </Layout>
+  );
+}
