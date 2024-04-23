a48a909a6051d732ca86e9ab4bb0c12db9ffd90e
danimisme <msubhanr53@gmail.com>
2024-04-23 22:31:05 +0700

    add modal slice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,12 @@ import { combineReducers } from "redux";
 
 import userLoggedReducer from "./slice/userLoggedSlice";
 import sidebarReducer from "./slice/sidebarSlice";
+import { modalUpdateRoleSlice } from "./slice/ModalUpdateRoleSlice";
 
 const rootReducer = combineReducers({
   userLogged: userLoggedReducer,
   sidebar: sidebarReducer,
+  modalUpdate: modalUpdateRoleSlice,
 });
 
 export const store = configureStore({
