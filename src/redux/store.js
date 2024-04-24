27406f010f37cac51de5610e0bb7c1c0ef26e931
danimisme <msubhanr53@gmail.com>
2024-04-24 12:37:06 +0700

    add form user reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import userLoggedReducer from "./slice/userLoggedSlice";
 import sidebarReducer from "./slice/sidebarSlice";
 import modalUpdateRoleReducer from "./slice/ModalUpdateRoleSlice";
+import formUserReducer from "./slice/FormUserSlice";
 
 const rootReducer = combineReducers({
   userLogged: userLoggedReducer,
   sidebar: sidebarReducer,
   modalUpdateRole: modalUpdateRoleReducer,
+  formUser: formUserReducer,
 });
 
 export const store = configureStore({
