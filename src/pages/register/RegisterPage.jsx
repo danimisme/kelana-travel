7aaea54fd8c48075a4f387843367642d40c4a5fe
danimisme <msubhanr53@gmail.com>
2024-04-24 22:20:20 +0700

    create handle register

--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -1,15 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import RegisterForm from "../../component/Fragments/RegisterForm/RegisterForm";
-
-export const metadata = {
-  title: "Register",
-  description: "Register to your account",
-};
+import useAuth from "../../hooks/useAuth";
 
 export default function RegisterPage() {
+  const { register } = useAuth();
+  const navigate = useNavigate();
+  const handleRegister = async (data) => {
+    try {
+      await register(data);
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="row d-flex vh-100 align-items-center justify-content-center">
       <div className="col-lg-6 col-md-8 offset-lg-3 col-10">
-        <RegisterForm />
+        <RegisterForm onSubmit={handleRegister} />
       </div>
     </div>
   );
