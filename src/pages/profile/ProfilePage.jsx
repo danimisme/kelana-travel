7b82efa60f8c51a06c8f6059e707da4e0349de8a
danimisme <msubhanr53@gmail.com>
2024-04-24 12:20:23 +0700

    modif styling for profile page

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import useAuth from "../../hooks/useAuth";
+import Layout from "../../layouts/Layout";
 export default function ProfilePage() {
   const { userLog } = useAuth();
   const [user, setUser] = useState({});
@@ -10,39 +12,47 @@ export default function ProfilePage() {
     });
   }, []);
   return (
-    <div className="mt-5 container-lg ">
-      <div className="row py-5 d-flex align-items-center">
-        <h2 className="text-center text-success mb-5"> My Profile </h2>
-        <div className="col-lg-6 col-10">
-          <img
-            src={user?.profilePictureUrl}
-            alt={user?.name}
-            className="img-fluid rounded mb-3"
-          />
+    <Layout>
+      <div className="mt-5 container-lg py-5">
+        <div
+          className="row bg-light py-2 rounded-lg mx-3"
+          style={{
+            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+            borderRadius: "15px",
+          }}
+        >
+          <h2 className="text-center text-orange my-2"> My Profile </h2>
+          <div className="col-lg-4 col-offset-lg-1 col-10 mx-auto p-3">
+            <img
+              src={user?.profilePictureUrl}
+              alt={user?.name}
+              className="img-fluid rounded mb-3 w-100"
+            />
+          </div>
+          <div className="col-lg-7 col-10 mx-auto p-3 d-flex flex-column justify-content-center">
+            <h3 className="text-orange mb-3">{user?.name}</h3>
+            <p className="fs-5 ">
+              <span className="fw-bold"> Email : </span>
+              {user?.email}
+            </p>
+            <p className="fs-5 ">
+              <span className="fw-bold"> Phone : </span>
+              {user?.phoneNumber}
+            </p>
+            <p className="fs-5 ">
+              <span className="fw-bold"> Role : </span>
+              {user?.role}
+            </p>
+            <button
+              className="default-button btn-orange w-50"
+              // onClick={() => dispatch(toggleFormUser())}
+            >
+              Edit Profile
+            </button>
+          </div>
         </div>
-        <div className="col-lg-6 col-10">
-          <h3>{user?.name}</h3>
-          <p>
-            <span className="fw-bold"> Email : </span>
-            {user?.email}
-          </p>
-          <p>
-            <span className="fw-bold"> Phone : </span>
-            {user?.phoneNumber}
-          </p>
-          <p>
-            <span className="fw-bold"> Role : </span>
-            {user?.role}
-          </p>
-          <button
-            className="btn btn-outline-success"
-            // onClick={() => dispatch(toggleFormUser())}
-          >
-            Edit Profile
-          </button>
-        </div>
+        {/* <EditUserForm user={user} /> */}
       </div>
-      {/* <EditUserForm user={user} /> */}
-    </div>
+    </Layout>
   );
 }
