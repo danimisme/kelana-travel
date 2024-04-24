404d5463e45cc37a0c9c5ddde9b56a39a4a1e4e6
danimisme <msubhanr53@gmail.com>
2024-04-24 23:23:52 +0700

    add text content

--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import RegisterForm from "../../component/Fragments/RegisterForm/RegisterForm";
 import useAuth from "../../hooks/useAuth";
 import "./RegisterPage.css";
@@ -19,9 +19,23 @@ export default function RegisterPage() {
 
   return (
     <main className="register_page">
-      <div className="row d-flex vh-100 align-items-center justify-content-center">
-        <div className="col-lg-6 col-md-8 offset-lg-3 col-10">
+      <div className="row d-flex vh-100 align-items-center ">
+        <div className="col-xl-4 col-md-8 offset-1 col-10">
           <RegisterForm onSubmit={handleRegister} />
+        </div>
+        <div className="col-xl-5 offset-xl-1 d-none d-xl-block">
+          <h1 className="text-white fs-1 fw-bold text_shadow">
+            Join our platform today to connect with travelers and expand your
+            reach
+          </h1>
+          <div className="d-flex align-items-center gap-3">
+            <h2 className="fw-bold text-white text_shadow">Or</h2>
+            <Link to="/" className="text-decoration-none ">
+              <button className="default-button btn-orange ">
+                Continue as Guest
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </main>
