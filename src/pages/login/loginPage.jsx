88a86f959d87a6853c0afe3b3396a919a61b591d
danimisme <msubhanr53@gmail.com>
2024-04-24 17:34:28 +0700

    add hrer to link

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -11,11 +11,12 @@ export default function LoginPage() {
         </div>
         <div className="col-lg-6 offset-md-1 col-md-5 col-10 my-5 text-white d-none d-md-block">
           <h1 className="text_shadow fs-1 fw-bold">
-            Unlock your travels! Enter your credentials to access Kelana Apps.
+            Unlock your travels! Enter your credentials to access Kelana Web
+            App.
           </h1>
           <div className="d-flex align-items-center gap-3">
             <h2 className="fw-bold">Or</h2>
-            <Link className="text-decoration-none ">
+            <Link to="/" className="text-decoration-none ">
               <button className="default-button btn-orange ">
                 Continue as Guest
               </button>
