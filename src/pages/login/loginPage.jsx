2295207a50cbca497ec1f831f5020e9df6ac29fe
danimisme <msubhanr53@gmail.com>
2024-04-24 17:28:16 +0700

    change style for login page

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import LoginForm from "../../component/Fragments/LoginForm/LoginForm";
 import "./LoginPage.css";
 
@@ -5,8 +6,21 @@ export default function LoginPage() {
   return (
     <main className="login_page">
       <div className="row d-flex vh-100 align-items-center justify-content-center">
-        <div className="col-lg-3 col-md-6 col-10 my-5 login_form">
+        <div className="col-lg-3 offset-md-1 col-md-4 col-10 my-5 login_form">
           <LoginForm />
+        </div>
+        <div className="col-lg-6 offset-md-1 col-md-5 col-10 my-5 text-white d-none d-md-block">
+          <h1 className="text_shadow fs-1 fw-bold">
+            Unlock your travels! Enter your credentials to access Kelana Apps.
+          </h1>
+          <div className="d-flex align-items-center gap-3">
+            <h2 className="fw-bold">Or</h2>
+            <Link className="text-decoration-none ">
+              <button className="default-button btn-orange ">
+                Continue as Guest
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </main>
