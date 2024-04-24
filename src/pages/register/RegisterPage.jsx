385ed25cae78eecc85f25946682df10b00c9662b
danimisme <msubhanr53@gmail.com>
2024-04-24 21:03:26 +0700

    create register Page

---
+++ src/pages/register/RegisterPage.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Register",
+  description: "Register to your account",
+};
+
+export default function RegisterPage() {
+  return (
+    <div className="row d-flex vh-100 align-items-center justify-content-center">
+      <div className="col-lg-6 col-md-8 offset-lg-3 col-10">
+        <h1>Register Page</h1>
+      </div>
+    </div>
+  );
+}
