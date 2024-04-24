ccb797c475975f3d1a653691cd6190b69ce87b07
danimisme <msubhanr53@gmail.com>
2024-04-24 16:26:56 +0700

    import css for styling

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -1,11 +1,14 @@
 import LoginForm from "../../component/Fragments/LoginForm/LoginForm";
+import "./LoginPage.css";
 
 export default function LoginPage() {
   return (
-    <div className="row d-flex vh-100 align-items-center justify-content-center">
-      <div className="col-lg-3 col-md-6 col-10 my-5">
-        <LoginForm />
+    <main className="login_page">
+      <div className="row d-flex vh-100 align-items-center justify-content-center">
+        <div className="col-lg-3 col-md-6 col-10 my-5">
+          <LoginForm />
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
