d797db49593b3d0770f2bf713a631dc80c1d5057
danimisme <msubhanr53@gmail.com>
2024-04-24 17:04:37 +0700

    add styling for login Page

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <main className="login_page">
       <div className="row d-flex vh-100 align-items-center justify-content-center">
-        <div className="col-lg-3 col-md-6 col-10 my-5">
+        <div className="col-lg-3 col-md-6 col-10 my-5 login_form">
           <LoginForm />
         </div>
       </div>
