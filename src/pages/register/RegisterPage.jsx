a7b064ec5cc9ca7dad27daaf95c7c5544a8a8403
danimisme <msubhanr53@gmail.com>
2024-04-24 21:17:25 +0700

    add register form to register page

--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -1,3 +1,5 @@
+import RegisterForm from "../../component/Fragments/RegisterForm/RegisterForm";
+
 export const metadata = {
   title: "Register",
   description: "Register to your account",
@@ -7,7 +9,7 @@ export default function RegisterPage() {
   return (
     <div className="row d-flex vh-100 align-items-center justify-content-center">
       <div className="col-lg-6 col-md-8 offset-lg-3 col-10">
-        <h1>Register Page</h1>
+        <RegisterForm />
       </div>
     </div>
   );
