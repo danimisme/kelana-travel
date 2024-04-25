94ffdebb489e54c0534ab99733260e13468696cf
danimisme <msubhanr53@gmail.com>
2024-04-25 22:16:29 +0700

    add modalDeleteSlice To Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,14 @@ import userLoggedReducer from "./slice/userLoggedSlice";
 import sidebarReducer from "./slice/sidebarSlice";
 import modalUpdateRoleReducer from "./slice/ModalUpdateRoleSlice";
 import formUserReducer from "./slice/FormUserSlice";
+import modalDeleteReducer from "./slice/ModalDeleteSlice";
 
 const rootReducer = combineReducers({
   userLogged: userLoggedReducer,
   sidebar: sidebarReducer,
   modalUpdateRole: modalUpdateRoleReducer,
   formUser: formUserReducer,
+  modalDelete: modalDeleteReducer,
 });
 
 export const store = configureStore({
