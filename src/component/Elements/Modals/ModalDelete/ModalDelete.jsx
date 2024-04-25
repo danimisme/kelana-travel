7cb6f2850772534226b0d6c053bd18e7956b1aed
danimisme <msubhanr53@gmail.com>
2024-04-25 22:08:21 +0700

    create modal delete

---
+++ src/component/Elements/Modals/ModalDelete/ModalDelete.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function ModalDelete() {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div
+      className={`modal_container ${isOpen ? "show_modal" : " hide_modal"} row`}
+    >
+      <div
+        className={`modal_dialog ${
+          isOpen ? "show_dialog" : "hide_dialog"
+        } col-lg-5 col-md-6 col-10 mx-auto`}
+      >
+        <div className="bg-light border-1 border-bottom mb-3">
+          <h3 className="text-orange">Delete</h3>
+        </div>
+        <div className="modal_body text-center">
+          <div className="my-3">
+            <p className="fw-bold fs-5">
+              Are you sure you want to delete this data?
+            </p>
+          </div>
+        </div>
+        <div className="modal_footer d-flex gap-3 mt-3 justify-content-end">
+          <button className="default-button btn-orange ">Confirm</button>
+          <button className="default-button" style={{ backgroundColor: "red" }}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
