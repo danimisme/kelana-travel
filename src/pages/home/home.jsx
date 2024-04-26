952faedc10fa393d4daca565ef96ad0242a039ff
danimisme <msubhanr53@gmail.com>
2024-04-26 23:46:48 +0700

    add animation in home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,13 +5,18 @@ import CategoryCarousel from "../../component/Fragments/CategoryCarousel/Categor
 import PromoCarousel from "../../component/Fragments/PromoCarousel/PromoCarousel";
 import Layout from "../../layouts/Layout";
 import "./home.css";
+import Animation from "../../utils/aos";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    Animation();
+  }, []);
   return (
     <Layout>
       <main className="mt-5 py-5">
         <div className="row d-flex align-items-center justify-content-center my-5 header-section">
-          <div className="col-lg-5  col-10 text-header ">
+          <div className="col-lg-5  col-10 text-header " data-aos="fade-right">
             <h1 className=" fs-1 fw-bold text-orange  m-3">
               Adventure to Explore <br /> Through the Beautiful World
             </h1>
@@ -21,17 +26,25 @@ export default function Home() {
               </button>
             </Link>
           </div>
-          <div className="col-lg-5 col-10 banner-container">
+          <div
+            className="col-lg-5 col-10 banner-container"
+            data-aos="fade-left"
+          >
             <BannerCarousel />
           </div>
         </div>
-        <div className="container-lg my-5 py-5 promo_section">
+        <div
+          className="container-lg my-5 py-5 promo_section"
+          data-aos="fade-up"
+        >
           <PromoCarousel />
         </div>
-        <div className="category_section py-5 my-5">
+        <div className="category_section py-5 my-5" data-aos="fade-up">
           <CategoryCarousel />
         </div>
-        <ActivityCarousel />
+        <div className="activity_section py-5 mt-5" data-aos="fade-up">
+          <ActivityCarousel />
+        </div>
       </main>
     </Layout>
   );
