ebc5819f8a78ec7bb63e8a4b3deb369761b9ee3e
danimisme <msubhanr53@gmail.com>
2024-04-28 16:35:26 +0700

    update protected role, only admin can acces dashboard

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,9 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
+  const user = useSelector((state) => state.userLogged.user);
+  console.log(user?.role);
   if (!token) {
     return <Navigate to="/login" />;
+  }
+
+  if (user?.role === "user") {
+    return <Navigate to="/" />;
   }
   return <>{children || <Outlet />}</>;
 }
