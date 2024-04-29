f7da098d78854069f2a9f21506467fe5e457032d
danimisme <msubhanr53@gmail.com>
2024-04-29 23:29:33 +0700

    create header section component

---
+++ src/component/Fragments/HeaderSection/HeaderSection.jsx
@@ -0,0 +1,9 @@
+export default function HeaderSection() {
+  return (
+    <div>
+      <h1 className="text-center fw-bold">
+        Welcome To <span className="text-orange">Kelana Travel</span>
+      </h1>
+    </div>
+  );
+}
