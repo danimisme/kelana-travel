ffa71d7bbf7fc6d8c69f88019bc76cae2421a5ee
danimisme <msubhanr53@gmail.com>
2024-05-02 21:58:18 +0700

    add conditional rendering to handle if data null

--- src/component/Fragments/CardAcivity/CardActivity.jsx
+++ src/component/Fragments/CardAcivity/CardActivity.jsx
@@ -20,34 +20,38 @@ export default function CardActivity({ activity, index }) {
         data-aos-once="true"
       >
         <img
-          src={activity.imageUrls[0]}
+          src={activity?.imageUrls[0]}
           className={` img card-img-top`}
           alt="..."
         />
         <p className="position-absolute top-0 end-0 bg-white m-1 px-2 py-1 rounded-pill ">
-          <i className="bi bi-star-fill text-warning"></i> {activity.rating}
+          <i className="bi bi-star-fill text-warning"></i> {activity?.rating}
         </p>
         <div className="card-body d-flex justify-content-between align-items-center">
           <div>
-            <h6 className={` title card-title`}>{activity.title}</h6>
+            <h6 className={` title card-title`}>{activity?.title}</h6>
             <p className={` text_location`}>
               <i className="bi bi-geo-alt-fill text-success"></i>
-              {` ${activity.city}, ${activity.province}`}
+              {` ${activity?.city}, ${activity?.province}`}
             </p>
           </div>
           <div>
-            <p className="p-0 m-0 text-decoration-line-through">
-              {activity.price.toLocaleString("id-ID", {
-                style: "currency",
-                currency: "IDR",
-              })}
-            </p>
-            <p>
-              {activity.price_discount.toLocaleString("id-ID", {
-                style: "currency",
-                currency: "IDR",
-              })}
-            </p>
+            {activity.price && (
+              <p className="p-0 m-0 text-decoration-line-through">
+                {activity?.price.toLocaleString("id-ID", {
+                  style: "currency",
+                  currency: "IDR",
+                })}
+              </p>
+            )}
+            {activity.price_discount && (
+              <p>
+                {activity?.price_discount.toLocaleString("id-ID", {
+                  style: "currency",
+                  currency: "IDR",
+                })}
+              </p>
+            )}
           </div>
         </div>
       </div>
